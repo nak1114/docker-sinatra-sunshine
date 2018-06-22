2fc973fda8186c411cd3af7aa26423128dadd07b
nak1114 <naktak1114@gmail.com>
2018-06-23 01:53:07 +0900

    Add auto-incriment to docker-image build process

--- Rakefile.rb
+++ Rakefile.rb
@@ -6,10 +6,12 @@ else
 
   name=File.basename(Dir.pwd)
   docker_run=%(docker run --rm -it -v "#{Dir.pwd}":/myapp -e RACK_ENV=development -e RUBYOPT=-EUTF-8 #{name})
+  def get_images(name) `docker images #{name}`.scan(/^#{name}\s+(\S+)\s+(\S+)/).map{|v| v[0]}; end
+  def get_new_version(name) (get_images(name).delete_if{|v| v=='latest'}.map{|v| Gem::Version.create(v)}.max || Gem::Version.create('0.0.0')).segments.tap{|v| v[-1]+=1}.join('.'); end
 
   desc "build new docker image"
   task :build , ['tag']  do |task, args|
-    tag=args[:tag] || "0.0.1"
+    tag=args[:tag] || get_new_version(name)
     touch('Gemfile.lock') unless File.exists?('Gemfile.lock')
     sh("docker build -t #{name}:#{tag} .")
     sh("docker tag #{name}:#{tag} #{name}:latest")
